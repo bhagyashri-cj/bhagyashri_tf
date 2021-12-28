4e67e3670555ecf2e3ea1968a3d41cc7634ba15e
bhagyashri-cj <bhagyashricjagtap@gmail.com>
2021-12-28 22:34:43 +0530

    These are my terraform files

---
+++ variable.tf
@@ -0,0 +1,38 @@
+variable "vpc_cidr" {
+
+    default = "10.0.0.0/16"
+}
+
+
+variable "subnets_cidr" {
+
+    default = "10.0.1.0/24"
+}
+
+
+variable "ami" {
+
+    default = "ami-0851b76e8b1bce90b"
+}
+
+
+
+
+variable "instance_type" {
+
+    default = "t2.micro"
+}
+
+
+
+variable "aws_key" {
+
+    default = "ohio_key"
+}
+
+
+variable "azs" {
+
+    default = "ap-south-1a"
+
+}
